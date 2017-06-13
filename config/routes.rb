8753fe315ac73902c7765f7349e8fd1ef21f13b3
Your Name <you@example.com>
2017-06-13 10:10:27 +0900

    issues/#11 add mail confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
   root 'top#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
