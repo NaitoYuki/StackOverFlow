872034a00ce04e6bfe013f7aaa94fdb3cba00cc4
Your Name <you@example.com>
2017-06-13 10:10:27 +0900

    issues/#11 add mail confirmation

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # ローカル環境でのメール確認gem
+  gem 'letter_opener_web'
 end
 
 # Bootstrapのgem
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 
   resources :questions
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
