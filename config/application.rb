15ccb27821383aff5e6315a5aa6d47faef7d3397
minoru_noda <minoru.noda.0527@gmail.com>
2017-06-14 22:23:58 +0900

    Add config not to create files under test directory

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module Stackoverflow
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # rails generate ... 時の設定
+    config.generators do |g|
+      g.test_framework false  # テストディレクトリ配下は作成しない
+    end
+
   end
 end
