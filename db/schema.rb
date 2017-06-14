a0dcad3bc0223b589e19a08464de101640e1a535
Your Name <you@example.com>
2017-06-14 22:38:41 +0900

    add user page

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170613115941) do
+ActiveRecord::Schema.define(version: 20170613130351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170613115941) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
+
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
@@ -62,8 +63,6 @@ ActiveRecord::Schema.define(version: 20170613115941) do
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
-
-  add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
