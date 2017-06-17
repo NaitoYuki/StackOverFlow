904eb0f89080d41185d3eb64f7a434c2c70ecb98
後藤研一郎 <gotokenichiro@gotoukenichirounoMacBook-Pro.local>
2017-06-18 01:53:09 +0900

    add questions counter within a week, a month

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
   def index
     @tags = Question.tags_on(:tags)
+    @questions_one_week = Question.where(created_at: 1.week.ago..Time.current)
+    @questions_one_month = Question.where(created_at: 1.month.ago..Time.current)
     if params[:search]
       @tags = @tags.where(['name LIKE ?', "%#{params[:search]}%"])
     end
