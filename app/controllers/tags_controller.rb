c44445543af2d3869e14495942c64961c17d4404
後藤研一郎 <gotokenichiro@gotoukenichirounoMacBook-Pro.local>
2017-06-17 23:49:17 +0900

    make rough design in tags#index page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,8 @@
 class TagsController < ApplicationController
   def index
     @tags = Question.tags_on(:tags)
+    if params[:search]
+      @tags = @tags.where(['name LIKE ?', "%#{params[:search]}%"])
+    end
   end
 end
