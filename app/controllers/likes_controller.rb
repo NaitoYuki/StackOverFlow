1604a122ebee1ac7e9b7075de42601065e0b11b3
Your Name <you@example.com>
2017-06-18 20:35:24 +0900

    heroku push レビュー後

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,10 +4,8 @@ class LikesController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @like = Like.new(question_id: params[:question_id], user_id: current_user.id)
-
     @like.save
-    render 'questions/show'
-
+    redirect_to question_path(params[:question_id])
   end
 
   def destroy
